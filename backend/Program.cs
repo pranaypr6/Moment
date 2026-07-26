477e88e8b2b901a9d4a55301a45a9bc1f65e1e59
Pranay Burra <pranayburra66@gmail.com>
2026-07-26 18:17:59 +0530

    fix: auto-parse Railway DATABASE_URL for Npgsql

--- backend/Program.cs
+++ backend/Program.cs
@@ -59,9 +59,17 @@ builder.Services.AddScoped<Moment.Api.Services.IReportService, Moment.Api.Servic
 builder.Services.AddSingleton<Moment.Api.Services.IStorageService, Moment.Api.Services.R2StorageService>();
 builder.Services.AddHostedService<Moment.Api.Workers.VibeCleanupWorker>();
 
-// Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var databaseUri = new Uri(databaseUrl);
+    var userInfo = databaseUri.UserInfo.Split(':');
+    connectionString = $"Host={databaseUri.Host};Port={databaseUri.Port};Database={databaseUri.LocalPath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};Ssl Mode=Prefer;Trust Server Certificate=true;";
+}
+
 builder.Services.AddDbContext<MomentDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 // Firebase
 var firebaseCredentialsBase64 = builder.Configuration["FIREBASE_CREDENTIALS_BASE64"] 
